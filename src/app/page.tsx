a28e325e842ef09a7125b17189824d5d4b8da1aa
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-09-12 14:23:37 +0100

    git commit -m "feat: implement navbar, update metadata, and install dependencies
    - Added a responsive navbar component
    - Updated metadata for improved SEO
    - Installed LucideReact for icons
    - Added ShadCN Button component"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import {
-  RegisterLink,
-  LoginLink,
-} from '@kinde-oss/kinde-auth-nextjs/components'
 export default function Home() {
-  return (
-    <div className="">
-      <LoginLink>Sign in</LoginLink>
-      <RegisterLink>Sign up</RegisterLink>
-    </div>
-  )
+  return <div className="">createpaste</div>
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,38 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { Metadata } from 'next'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function constructMetadata({
+  title = 'SecurPaste',
+  description = 'A secure and user-friendly Pastebin clone allowing users to create, store, and share text snippets with personal accounts.',
+  image = '',
+  icons = '/favicon.ico',
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: 'kkalsfgioa',
+      title,
+      description,
+      images: [image],
+      creator: '@iamfinzyphinzy_',
+    },
+    icons,
+    metadataBase: new URL('https://securpaste.vercel.app/'),
+  }
 }
